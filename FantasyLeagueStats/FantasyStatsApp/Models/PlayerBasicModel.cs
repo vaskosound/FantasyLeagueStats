ab8d487075a8fc6366bce6e2cd2cd0617bbd2c1c
Vasil Lazarov <vaskosound@abv.bg>
2013-11-18 15:26:49 -0800

    Added PlayerBasicModel

---
+++ FantasyLeagueStats/FantasyStatsApp/Models/PlayerBasicModel.cs
@@ -0,0 +1,51 @@
+﻿using FantasyStats.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace FantasyStatsApp.Models
+{
+    public class PlayerBasicModel
+    {
+        public static Expression<Func<Player, PlayerBasicModel>> FromPlayersStats =
+           player => new PlayerBasicModel()
+           {
+               Id = player.Id,
+               Name = player.Name,
+               Team = player.Team.Initials,
+               Selected = player.Selected,
+               Price = player.Price,
+               Position = player.Position,
+               Points = player.Points,
+               MinutesPlayed = player.MinutesPlayed,
+               PointsPerGame = player.PointsPerGame
+           };
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Team { get; set; }
+
+        public double Selected { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Points { get; set; }
+                
+        public Position Position { get; set; }
+
+        public string PositionName
+        {
+            get
+            {
+                return this.Position.ToString();
+            }
+        }
+
+        public int MinutesPlayed { get; set; }
+
+        public decimal PointsPerGame { get; set; }
+    }
+}
