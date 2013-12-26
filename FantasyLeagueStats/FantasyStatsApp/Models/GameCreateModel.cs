af06156dc5de26543b0dbf53c604cbf4343ba8ed
Vasil Lazarov <vaskosound@abv.bg>
2013-12-26 15:59:38 -0800

    Fixed create game model

--- FantasyLeagueStats/FantasyStatsApp/Models/GameCreateModel.cs
+++ FantasyLeagueStats/FantasyStatsApp/Models/GameCreateModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace FantasyStatsApp.Models.GameModels
+namespace FantasyStatsApp.Models
 {
     public class GameCreateModel
     {
